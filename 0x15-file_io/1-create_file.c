e8fab7e48a0d1803db4e6bbcb9ff1c22f077bb6b
amine-13 <aminegbadamassi@gmail.com>
2023-04-14 18:07:30 +0000

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,43 +1,36 @@
 #include "main.h"
 
 /**
- * create_file - creates a file with given content
- * @filename: the name of the file to create
- * @text_content: the content to put in the file
- *
- * Return: 1 on success, -1 on failure
- */
+  * create_file - ...
+  * @filename: ...
+  * @text_content: ...
+  *
+  * Return: ...
+  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, num_written;
+	int fd;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL && *text_content != '\0')
-	{
-		num_written = write(fd, text_content, _strlen(text_content));
-		if (num_written == -1 || num_written != _strlen(text_content))
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+	if (text_content)
+		write(fd, text_content, _strlen(text_content));
 
 	close(fd);
 	return (1);
 }
 
 /**
- * _strlen - Returns the length of a string
- * @s: String to count
- *
- * Return: String length
- */
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
 int _strlen(char *s)
 {
 	int c = 0;
