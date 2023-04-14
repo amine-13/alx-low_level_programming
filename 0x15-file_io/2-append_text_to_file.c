dd2992b1d1ffe12e35c3976b583302ea26dc5db6
amine-13 <aminegbadamassi@gmail.com>
2023-04-14 18:37:56 +0000

    Update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,38 +2,27 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends text to the end of a file
- * @filename: the name of the file to append to
- * @text_content: the text to append
- *
- * Return: 1 on success, -1 on failure
- */
+  * append_text_to_file - ...
+  * @filename: ...
+  * @text_content: ...
+  *
+  * Return: ...
+  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, num_written;
+	int fd;
 
 	if (!filename)
-	{
-		fprintf(stderr, "Error: filename is null\n");
 		return (-1);
-	}
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
-	{
-		fprintf(stderr, "Error: could not open file %s\n", filename);
 		return (-1);
-	}
 
 	if (text_content)
 	{
-		num_written = write(fd, text_content, _strlen(text_content));
-		if (num_written == -1 || num_written != _strlen(text_content))
-		{
-			fprintf(stderr, "Error: could not write to file %s\n", filename);
-			close(fd);
+		if (write(fd, text_content, _strlen(text_content)) == -1)
 			return (-1);
-		}
 	}
 
 	close(fd);
@@ -41,11 +30,11 @@ int append_text_to_file(const char *filename, char *text_content)
 }
 
 /**
- * _strlen - Returns the length of a string
- * @s: String to count
- *
- * Return: String length
- */
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
 int _strlen(char *s)
 {
 	int c = 0;
