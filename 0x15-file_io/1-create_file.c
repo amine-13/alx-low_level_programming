fa158fe9993a8d9f346a15ed26813a8601cf8a9f
amine-13 <aminegbadamassi@gmail.com>
2023-04-14 18:06:21 +0000

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
  * create_file - creates a file with given content
@@ -16,14 +14,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL && *text_content != '\0')
 	{
-		num_written = write(fd, text_content, strlen(text_content));
-		if (num_written == -1 || num_written != strlen(text_content))
+		num_written = write(fd, text_content, _strlen(text_content));
+		if (num_written == -1 || num_written != _strlen(text_content))
 		{
 			close(fd);
 			return (-1);
@@ -32,4 +30,20 @@ int create_file(const char *filename, char *text_content)
 
 	close(fd);
 	return (1);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
 }
